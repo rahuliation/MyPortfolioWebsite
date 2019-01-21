f9e68408caccd8499db38b5b610bf903742240ad
rahuliation <rahuliation@gmail.com>
2019-01-22 03:55:30 +0600

    working

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+const Home = () => (<div>
+    <dl class="mt2 f6 lh-copy">
+        <dt class="clip">Title</dt>
+        <dd class="ml0 black truncate w-100">Everybody Looking</dd>
+        <dt class="clip">Artist</dt>
+        <dd class="ml0 gray truncate w-100">Gucci Mane</dd>
+    </dl>
+</div>);
+
+export default Home;
---
+++ src/pages/Resume.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Resume = () => <div>
+    <div className="w-100 w-50-ns center">
+        <h3>Resume</h3>
+        <a href='https://docs.google.com/document/d/1AVwUQXhdXxwCX2ioL7IrUDo5rnO1vC1WZ61tfKW0t7I/export?format=pdf'>Download as PDF</a>
+
+        <iframe className="resume" src="https://docs.google.com/document/d/1AVwUQXhdXxwCX2ioL7IrUDo5rnO1vC1WZ61tfKW0t7I/view" />
+    </div>
+</div>;
+
+export default Resume;
