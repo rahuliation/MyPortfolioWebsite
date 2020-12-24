8e5ea472934ffbe24ce1e29831cc8cdf8168b3f4
rahul rahman <rahuliation@gmail.com>
2020-12-24 13:41:25 +0600

    staticData Separate;

--- src/Layouts/MyLayout.tsx
+++ src/Layouts/MyLayout.tsx
@@ -1,149 +1,162 @@
-import * as React from 'react';
-import { withRouter, RouteComponentProps } from 'react-router-dom'
-import { Link } from 'react-router-dom'
-import Typing from 'react-typing-animation';
+import * as React from "react";
+import { withRouter, RouteComponentProps } from "react-router-dom";
+import { Link } from "react-router-dom";
+import Typing from "react-typing-animation";
+import staticData from "../staticData";
 
-const MenuItem = (props: { in?: number, active: boolean, name: string, icon: string, path: string }) => (
-  props.active ?
-    (<span className="f7 f5-ns ttc tracked-mega link dib pv2-ns ph4-ns mb2 pv1 ph2 b menuActive primary pointer" key={props.in}>
-      <div className={`b`}>
-        <i className={`f3-l ${props.icon}`}/> <br />
-        <span className="ttu tracked"> {props.name}</span>
-      </div>
-    </span>)
-    :
-    (<Link 
-      className="f7 f5-ns ttc tracked-mega link dib pv2-ns ph4-ns pv1 ph2 mid-gray glow pointer"
-      to={props.path || '/'}
+const MenuItem = (props: {
+  in?: number;
+  active: boolean;
+  name: string;
+  icon: string;
+  path: string;
+}) =>
+  props.active ? (
+    <span
+      className="f7 f5-ns ttc tracked-mega link dib pv2-ns ph4-ns mb2 pv1 ph2 b menuActive primary pointer"
       key={props.in}
     >
       <div className={`b`}>
-        <i className={`f3-l ${props.icon}`}/> <br />
+        <i className={`f3-l ${props.icon}`} /> <br />
+        <span className="ttu tracked"> {props.name}</span>
+      </div>
+    </span>
+  ) : (
+    <Link
+      className="f7 f5-ns ttc tracked-mega link dib pv2-ns ph4-ns pv1 ph2 mid-gray glow pointer"
+      to={props.path || "/"}
+      key={props.in}
+    >
+      <div className={`b`}>
+        <i className={`f3-l ${props.icon}`} /> <br />
         <span className="ttu tracked-mega"> {props.name}</span>
       </div>
-    </Link>)
-)
-
+    </Link>
+  );
 
 const Menu = (props: any) => (
   <div className="w-100 bg-white shadow-2 navy mmenu pv2" ref={props.navRef}>
     <nav className="tc mw7 center">
-      {
-        props.menus.map((menu: any, key: number) => (
-          <MenuItem
-            {...menu}
-            in={key}
-            active={location.pathname === menu.path}
-          />
-        ))
-      }
+      {props.menus.map((menu: any, key: number) => (
+        <MenuItem {...menu} in={key} active={location.pathname === menu.path} />
+      ))}
     </nav>
   </div>
 );
 
-export const Social = (props: { className?: string, hoverClass?: string }) =>  ( <div className={`${props.className} social-icons`}>
-{[{
-  href: 'https://www.linkedin.com/in/rahul-workspace/',
-  icon: 'uil uil-linkedin-alt'
-},{
-  href: 'https://facebook.com/rahuliation',
-  icon: 'uil uil-facebook-f'
-},
-{
-  href: 'https://github.com/rahuliation',
-  icon: 'uil uil-github-alt'
-},
-{
-  href: 'mailto:mail@rahul.com.bd?Subject=Hello',
-  icon: 'uil uil-envelope-alt'
-},
-{
-  href: 'skype:rahul.workspace@gmail.com?call',
-  icon: 'uil uil-chat'
-}
-].map((val, key) => (<a href={val.href} 
-target="_blank" 
-className={`ba br-100 tc mr2 ${props.hoverClass}  b bw1 `}
-key={key}
-> 
-<i className={`grow ${val.icon} f5`} />
-</a>))}
-</div>)
+export const Social = (props: { className?: string; hoverClass?: string }) => (
+  <div className={`${props.className} social-icons`}>
+    {[
+      {
+        href: "https://www.linkedin.com/in/rahul-workspace/",
+        icon: "uil uil-linkedin-alt",
+      },
+      {
+        href: "https://facebook.com/rahuliation",
+        icon: "uil uil-facebook-f",
+      },
+      {
+        href: "https://github.com/rahuliation",
+        icon: "uil uil-github-alt",
+      },
+      {
+        href: "mailto:mail@rahul.com.bd?Subject=Hello",
+        icon: "uil uil-envelope-alt",
+      },
+      {
+        href: "skype:rahul.workspace@gmail.com?call",
+        icon: "uil uil-chat",
+      },
+    ].map((val, key) => (
+      <a
+        href={val.href}
+        target="_blank"
+        className={`ba br-100 tc mr2 ${props.hoverClass}  b bw1 `}
+        key={key}
+      >
+        <i className={`grow ${val.icon} f5`} />
+      </a>
+    ))}
+  </div>
+);
 
-const layout = ({ history, children, location }: { children: JSX.Element } & RouteComponentProps) => {
-
+const layout = ({
+  history,
+  children,
+  location,
+}: { children: JSX.Element } & RouteComponentProps) => {
   const { menus, scrollRef, scrollRef2, navRef } = layoutHooks(history);
 
   return (
     <div className="fl w-100 bg-white2" dir="ltr">
-      <div
-        className="pv4 ph2 bg-navy2 near-white pv6-l fixed w-25-l w-100 vh-100 sidebar"
-      >
-          <div className="w4-ns w4 center mb3">
-            <img className="br-100 profilePic" src="/profilepic.jpg" alt="Rahul" />
-          </div>
-          <div className="tc" style={{ fontFamily: 'Viga, sans-serif', fontWeight: 300 }}>
-            <span className="f1-ns f3" >Rahul Islam</span>
+      <div className="pv4 ph2 bg-navy2 near-white pv6-l fixed w-25-l w-100 vh-100 sidebar">
+        <div className="w4-ns w4 center mb3">
+          <img
+            className="br-100 profilePic"
+            src="/profilepic.jpg"
+            alt="Rahul"
+          />
+        </div>
+        <div
+          className="tc"
+          style={{ fontFamily: "Viga, sans-serif", fontWeight: 300 }}
+        >
+          <span className="f1-ns f3">{staticData.name}</span>
+          <br />
+          <span className="f3-ns f5 pv0"> Software Engineer </span>
+          <div className="mt5-l mt4-m mt3 tc f2-ns f3 pv0 hidden-h400">
+            <span className="f3-ns f4">I work with </span>
             <br />
-            <span className="f3-ns f5 pv0"> Software Engineer </span>
-            <div className="mt5-l mt4-m mt3 tc f2-ns f3 pv0 hidden-h400">
-              <span className="f3-ns f4">I work with </span>
-              <br />
-              <Typing loop={true} className="dib" cursorClassName="white">
-                {
-                  [
-                    'NodeJS.',
-                    'Graphql.',
-                    'ExpressJS.',
-                    'ReactJS.',
-                    'FeatherJS.',
-                    'VueJS.',
-                    'PHP, Laravel.'
-                  ].map(text => (
-                    <>
-                      <span className="b">{text}</span>
-                      <Typing.Delay ms={3000} />
-                      <Typing.Backspace count={text.length} />
-                    </>
-                  ))
-                }
-                <Typing.Reset />
-              </Typing>
-            </div>
+            <Typing loop={true} className="dib" cursorClassName="white">
+              {staticData.frontSkil.map((text) => (
+                <>
+                  <span className="b">{text}</span>
+                  <Typing.Delay ms={3000} />
+                  <Typing.Backspace count={text.length} />
+                </>
+              ))}
+              <Typing.Reset />
+            </Typing>
           </div>
+        </div>
         <br />
         <div className="absolute hidden-h500 w-100 bottom-2 overflow-x-hidden tc">
-          <a href={process.env.REACT_APP_CV_DOWNLOAD_PDF}
+          <a
+            href={process.env.REACT_APP_CV_DOWNLOAD_PDF}
             className="grow  ph3 hover-white sans-serif w-100 db pv2 b bb button near-white f5"
-            style={{ bottom: '5rem' }}>
-            <i className='im im-download f7' /> DOWNLOAD CV
+            style={{ bottom: "5rem" }}
+          >
+            <i className="im im-download f7" /> DOWNLOAD CV
           </a>
-          <Social className="f6 pt3 w-100 cf" hoverClass="hover-bg-white hover-navy" />
+          <Social
+            className="f6 pt3 w-100 cf"
+            hoverClass="hover-bg-white hover-navy"
+          />
         </div>
       </div>
-      <div className="fl w-100 mbody bg-white2 "  ref={scrollRef} >
+      <div className="fl w-100 mbody bg-white2 " ref={scrollRef}>
         <Menu menus={menus} navRef={navRef} />
-        <div className="fl w-100 fadeIn animated" ref={scrollRef2} >
+        <div className="fl w-100 fadeIn animated" ref={scrollRef2}>
           {children}
         </div>
       </div>
     </div>
   );
-}
+};
 
 const layoutHooks = (history: any) => {
   const [menus] = React.useState([
     {
-      key: 'home',
-      name: 'Home',
-      icon: 'uil uil-home',
-      path: '/',
+      key: "home",
+      name: "Home",
+      icon: "uil uil-home",
+      path: "/",
     },
     {
-      key: 'resume',
-      name: 'Resume',
-      icon: 'uil uil-document-layout-right',
-      path: '/resume',
+      key: "resume",
+      name: "Resume",
+      icon: "uil uil-document-layout-right",
+      path: "/resume",
     },
     // {
     //   key: 'blog',
@@ -152,34 +165,33 @@ const layoutHooks = (history: any) => {
     //   path: '/blog',
     // },
     {
-      key: 'portfolio',
-      name: 'Portfolio',
-      icon: 'uil uil-suitcase-alt',
-      path: '/portfolio',
+      key: "portfolio",
+      name: "Portfolio",
+      icon: "uil uil-suitcase-alt",
+      path: "/portfolio",
     },
     {
-      key: 'contact',
-      name: 'Contact',
-      icon: 'uil uil-phone',
-      path: '/contact',
-    }
+      key: "contact",
+      name: "Contact",
+      icon: "uil uil-phone",
+      path: "/contact",
+    },
   ]);
-  const scrollRef = React.useRef<any>(null) // Hook to ref object
-  const scrollRef2 = React.useRef<any>(null) // Hook to ref object
-  const navRef = React.useRef<any>(null) // Hook to ref object
-
+  const scrollRef = React.useRef<any>(null); // Hook to ref object
+  const scrollRef2 = React.useRef<any>(null); // Hook to ref object
+  const navRef = React.useRef<any>(null); // Hook to ref object
 
   history.listen((res: any) => {
     if (scrollRef.current && scrollRef.current.offsetTop) {
       window.scrollTo({
         top: scrollRef.current.offsetTop,
       });
-    } else if(scrollRef2.current && scrollRef2.current.offsetTop){
+    } else if (scrollRef2.current && scrollRef2.current.offsetTop) {
       window.scrollTo({
         top: scrollRef2.current.offsetTop - navRef.current.offsetHeight,
       });
     }
   });
-  return { menus, scrollRef, scrollRef2, navRef }
-}
+  return { menus, scrollRef, scrollRef2, navRef };
+};
 export const MyLayout = withRouter(layout);
--- src/Pages/Resume.tsx
+++ src/Pages/Resume.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
-export const Resume = () => {
-  const { experiences, educations, skills } = ResumeHooks();
-  return (
+import staticData from '../staticData';
+
+export const Resume = () => (
     <div className="fl w-100 gotham primary pb4 ph3-ns ph2">
       <div className="cf center w-100 bg-white br4 shadow-custom mt4 pv3 mw-custom">
         <div className="fl w-100 tc mt3 mb4 pt4">
@@ -9,7 +9,7 @@ export const Resume = () => {
         </div>
         <div className="flex flex-wrap pa3">
           {
-            skills.map((skill, key) => (<div className="w-20-l w-third-m w-50 flex-auto pa2" key={key}>
+            staticData.skills.map((skill, key) => (<div className="w-20-l w-third-m w-50 flex-auto pa2" key={key}>
               <article className="bg-white br3 pa3 ba grow b--black-10 tc"  >
                 <i className={`h3 w3 dib f1-ns f2 ${skill.icon}`} />
                 <h2 className="f4-ns f6 fw4 gray mt0">{skill.title}</h2>
@@ -28,7 +28,7 @@ export const Resume = () => {
         <div className="fl pl4 w-100 pv3">
           <section className="timeline-section pv4">
             {
-              experiences.map((exp: any, key) =>
+              staticData.experience.map((exp: any, key) =>
                 (<article
                   className="relative timeline-item ph4 "
                   data-aos="fade-up"
@@ -77,7 +77,7 @@ export const Resume = () => {
         <div className="fl pl4 w-100 pv3">
           <section className="timeline-section pv4">
             {
-              educations.map((edu: any, key) =>
+              staticData.educations.map((edu: any, key) =>
                 (<article
                   className="relative timeline-item ph4 glow "
                   data-aos="fade-up"
@@ -138,137 +138,3 @@ export const Resume = () => {
       </div>
     </div>
   );
-};
-
-const ResumeHooks = () => {
-  const [experiences] = React.useState([{
-    timeline: 'June, 2020 - Present',
-    companyName: 'Ixora Solution Limited',
-    designition: 'Sr. Software Engineer',
-    location: 'Uttara, Dhaka, Bangladesh',
-    link: 'https://ixorasolution.com',
-    details: 'Involved in some american project. Here FeatherJS , ReactJS, Mobx are our stacks'
-  }, {
-    timeline: 'December, 2018 - May 2020',
-    companyName: 'INVARIANT TELECOM',
-    designition: 'Software Engineer',
-    location: 'Mohakhali, Dhaka, Bangladesh',
-    link: 'https://invarianttele.com/',
-    details: 'I have already involved in 3 wonderful service like Dhamaka Digital, BUDDY and Wallet Service . Dhamaka is Multivendor Ecommerce Solution. Buddy is a ride sharing service and Wallet is a financial service. I contributed myself working with various technology like NodeJS, GraphQL, ExpressJS, MongoDB, VueJS. Build Restfull api and Back office pannels'
-  },
-  {
-    timeline: 'March, 2018 - November, 2018',
-    companyName: 'DOCTORKOI SMART',
-    designition: 'Full Stack Javascript Developer',
-    location: 'Mohammadpur, Dhaka, Bangladesh',
-    link: 'https://www.doctorkoi.com/',
-    details: 'Doctorkoi SMART is prescription management software that helps Doctors to generate prescriptions within just few clicks. It requires few seconds to prepare a full prescription by using SMART. Doctors can customize prescription according to their needs. Medicines can be selected from 26,000+ registered drug lists. So there is no chance to make any mistake. I worked as a full stack javascript developer. ReactJS, Featherjs, ElectronJS and many more things are used'
-  },
-  {
-    timeline: 'January, 2017 - February, 2018',
-    companyName: 'UY SYSTEM LTD',
-    designition: 'Web Developer',
-    link: 'https://www.uysys.com/',
-    location: 'Mohakhali, Dhaka, Bangladesh',
-    details: 'Its an outsourcing software company. I have done multiple projects of outsourcing clients. Project of tourism management like tripzip.tours and Ecommerce Solution is done by me . NodeJS , Laravel , ExpressJS, Mysql , Mongodb are used for providing solutions'
-  }]);
-
-
-
-
-
-
-  const [skills] = React.useState<{ icon: string, title: string }[]>([
-    {
-      icon: 'devicon-javascript-plain colored',
-      title: 'Javascript',
-    }, {
-      icon: 'devicon-nodejs-plain colored',
-      title: 'NodeJS',
-    }, {
-      icon: 'devicon-express-original colored',
-      title: 'Express',
-    }, {
-      icon: 'devicon-react-original colored',
-      title: 'ReactJS',
-    }, {
-      icon: 'devicon-meteor-plain colored',
-      title: 'MetorJS'
-    },
-    {
-      icon: 'devicon-vuejs-plain colored',
-      title: 'VueJS',
-    }, {
-      icon: 'devicon-bootstrap-plain colored',
-      title: 'Bootstrap',
-    }, {
-      icon: 'devicon-php-plain colored',
-      title: 'PHP',
-    }, {
-      icon: 'devicon-laravel-plain colored',
-      title: 'Laravel',
-    }, {
-      icon: 'devicon-java-plain colored',
-      title: 'JAVA',
-    }, {
-      icon: 'devicon-ruby-plain colored',
-      title: 'Ruby'
-    }, {
-      icon: 'devicon-angularjs-plain colored',
-      title: 'AngularJS',
-    }, {
-      icon: 'devicon-mysql-plain colored',
-      title: 'MySQL',
-    }, {
-      icon: 'devicon-postgresql-plain colored',
-      title: 'PostgreSQL',
-    }, {
-      icon: 'devicon-redis-plain colored',
-      title: 'Redis',
-    }, {
-      icon: 'devicon-mongodb-plain colored',
-      title: 'MongoDB'
-    }, {
-      icon: 'devicon-docker-plain colored',
-      title: 'Docker',
-    }, {
-      icon: 'devicon-git-plain colored',
-      title: 'Git',
-    }, {
-      icon: 'devicon-nginx-original colored',
-      title: 'Nginx'
-    }, {
-      icon: 'devicon-linux-plain colored',
-      title: 'Linux'
-    }
-  ]);
-
-
-  const [educations] = React.useState([{
-    timeline: '2013-2016',
-    institute: 'INTERNATIONAL UNIVERSITY OF BUSINESS AGRICULTURE AND TECHNOLOGY (IUBAT)',
-    degree: 'Bachelor of Computer Science and Engineering',
-    result: 'CGPA 3.19 (out of 4)',
-    board: null,
-    location: 'Uttara, Dhaka, Bangladesh'
-  },
-  {
-    timeline: '2009 - 2012',
-    institute: 'DHAKA POLYTECHNIC INSTITUTE',
-    degree: 'Diploma in Engineering',
-    result: null,
-    board: 'Dhaka Board',
-    location: 'Tejgaon, Dhaka, Bangladesh'
-  },
-  {
-    timeline: '2006-2008',
-    institute: 'MIRPUR BANGLA HIGH SCHOOL',
-    degree: 'Secondary School Certificate (SSC)',
-    result: 'GPA 4.5 (out of 5)',
-    board: 'Dhaka Board',
-    location: 'Mirpur, Dhaka, Bangladesh'
-  }]);
-
-
-  return { experiences, educations, skills };
-}
