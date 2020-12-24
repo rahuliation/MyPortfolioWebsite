d45e26569d45c2c3612de69be0be9738da91dd62
Rahul Islam <rahuliation@gmail.com>
2020-12-24 13:50:06 +0600

    staticData Separate; (#9)

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,18 +1,28 @@
-import * as React from 'react';
+import * as React from "react";
+import staticData from "../staticData";
 
-export const Home = () =>  (
-        <div className={`fl w-100 primary pb4 ph3-ns ph2`}>
-            <div className="cf tc center w-100 bg-white br4 shadow-custom mt4 pv5 ph4 mw-custom">
-                <span className="db center mw6 bg-navy2 gothamMedium  white pa3 f2-ns f4 br3 fw7 pa3 lh-title mv0 tracked-tight">
-                        Hello, I am  Rahul Islam
-                </span>
-                <span className="db pb3 f3-l f4-m f5 mt5 fw1 i mid-gray">A Full Stack <b>NodeJS and ReactJS</b>  Web/Software Developer</span>
-                <span className="db f5 gotham tracked black-80"> <i className="im im-location f6" /> Dhaka, Bangladesh.</span>
-                <div className="center h7">
-                    <p className="f4-ns f5 lh-copy">I have 2 years 6 months working experience in different software company. Javascript, nodeJS Mongodb, express, featherjs, React, react native, electronjs are my top key skills and gathered experince through invoving myself in development of many desktop application , Web application.</p>
-                    <p className="f4-ns f5 lh-copy">Started my career as a Software Engineer and I got the opportunity to work in different companies with different teams and technologies. From there, I  have learned a lot about Research and Development which is a key factor in order to solve newer problems.</p>
-                    <p className="f4-ns f5 lh-copy">Having a Computer Science background, I look forward to learning and research more and more about new technology and ready to face all kinds of challenges with a view to establishing myself as someone with excellent creativity, professional skills and leadership abilities which I would love to implement in real life for making this world a much better place.</p>
-                </div>
-            </div>
-        </div>
-    );
+export const Home = () => (
+  <div className={`fl w-100 primary pb4 ph3-ns ph2`}>
+    <div className="cf tc center w-100 bg-white br4 shadow-custom mt4 pv5 ph4 mw-custom">
+      <span className="db center mw6 bg-navy2 gothamMedium  white pa3 f2-ns f4 br3 fw7 pa3 lh-title mv0 tracked-tight">
+        {staticData.title}
+      </span>
+      <span className="db pb3 f3-l f4-m f5 mt5 fw1 i mid-gray" dangerouslySetInnerHTML={{
+          __html: staticData.subtitle
+      }}/>
+        
+      <span className="db f5 gotham tracked black-80">
+        <i className="im im-location f6" /> {staticData.location}
+      </span>
+      <div
+        className="center h7 f4-ns f5 lh-copy"
+        dangerouslySetInnerHTML={{
+          __html: staticData.description.replace(
+            "%year%",
+            (new Date().getMonth() > 8 ? new Date().getFullYear() - 2016 : new Date().getFullYear() - 2017).toString()
+          ),
+        }}
+      />
+    </div>
+  </div>
+);
---
+++ src/staticData.js
@@ -0,0 +1,177 @@
+export default {
+    name: "Rahul Rahman",
+    title: "Hello, I am Rahul Rahman",
+    subtitle: `A Full Stack <b>NodeJS and ReactJS</b> Web/Software Developer`,
+    location: `Dhaka., Bangladesh`,
+    frontSkil: [
+        "NodeJS",
+        "Graphql",
+        "ExpressJS",
+        "ReactJS",
+        "FeathersJS",
+        "VueJS",
+        "ROR",
+        "PHP, Laravel",
+        "Electron JS",
+        "Mongo DB",
+      ],
+    description: `<p className="f4-ns f5 lh-copy">I have %year%+ working experience in different software company. Javascript, nodeJS Mongodb, express, featherjs, React, react native, electronjs are my top key skills and gathered experince through invoving myself in development of many desktop application , Web application.</p>
+    <p className="f4-ns f5 lh-copy">Started my career as a Software Engineer and I got the opportunity to work in different companies with different teams and technologies. From there, I  have learned a lot about Research and Development which is a key factor in order to solve newer problems.</p>
+    <p className="f4-ns f5 lh-copy">Having a Computer Science background, I look forward to learning and research more and more about new technology and ready to face all kinds of challenges with a view to establishing myself as someone with excellent creativity, professional skills and leadership abilities which I would love to implement in real life for making this world a much better place.</p>`,
+    educations: [{
+        timeline: '2013-2016',
+        institute: 'INTERNATIONAL UNIVERSITY OF BUSINESS AGRICULTURE AND TECHNOLOGY (IUBAT)',
+        degree: 'Bachelor of Computer Science and Engineering',
+        result: 'CGPA 3.19 (out of 4)',
+        board: null,
+        location: 'Uttara, Dhaka, Bangladesh'
+      },
+      {
+        timeline: '2009 - 2012',
+        institute: 'DHAKA POLYTECHNIC INSTITUTE',
+        degree: 'Diploma in Engineering',
+        result: null,
+        board: 'Dhaka Board',
+        location: 'Tejgaon, Dhaka, Bangladesh'
+      },
+      {
+        timeline: '2006-2008',
+        institute: 'MIRPUR BANGLA HIGH SCHOOL',
+        degree: 'Secondary School Certificate (SSC)',
+        result: 'GPA 4.5 (out of 5)',
+        board: 'Dhaka Board',
+        location: 'Mirpur, Dhaka, Bangladesh'
+      }],
+      skills : [
+        {
+          icon: 'devicon-javascript-plain colored',
+          title: 'Javascript',
+        }, {
+          icon: 'devicon-nodejs-plain colored',
+          title: 'NodeJS',
+        }, {
+          icon: 'devicon-express-original colored',
+          title: 'Express',
+        }, {
+          icon: 'devicon-react-original colored',
+          title: 'ReactJS',
+        }, {
+          icon: 'devicon-meteor-plain colored',
+          title: 'MetorJS'
+        },
+        {
+          icon: 'devicon-vuejs-plain colored',
+          title: 'VueJS',
+        }, {
+          icon: 'devicon-bootstrap-plain colored',
+          title: 'Bootstrap',
+        }, {
+          icon: 'devicon-php-plain colored',
+          title: 'PHP',
+        }, {
+          icon: 'devicon-laravel-plain colored',
+          title: 'Laravel',
+        }, {
+          icon: 'devicon-java-plain colored',
+          title: 'JAVA',
+        }, {
+          icon: 'devicon-ruby-plain colored',
+          title: 'Ruby'
+        }, {
+          icon: 'devicon-angularjs-plain colored',
+          title: 'AngularJS',
+        }, {
+          icon: 'devicon-mysql-plain colored',
+          title: 'MySQL',
+        }, {
+          icon: 'devicon-postgresql-plain colored',
+          title: 'PostgreSQL',
+        }, {
+          icon: 'devicon-redis-plain colored',
+          title: 'Redis',
+        }, {
+          icon: 'devicon-mongodb-plain colored',
+          title: 'MongoDB'
+        }, {
+          icon: 'devicon-docker-plain colored',
+          title: 'Docker',
+        }, {
+          icon: 'devicon-git-plain colored',
+          title: 'Git',
+        }, {
+          icon: 'devicon-nginx-original colored',
+          title: 'Nginx'
+        }, {
+          icon: 'devicon-linux-plain colored',
+          title: 'Linux'
+        }
+      ],
+      experience: [{
+        timeline: 'June, 2020 - Present',
+        companyName: 'Ixora Solution Limited',
+        designition: 'Sr. Software Engineer',
+        location: 'Uttara, Dhaka, Bangladesh',
+        link: 'https://ixorasolution.com',
+        details: 'Involved in some american project. Here FeatherJS , ReactJS, Mobx are our stacks'
+      }, {
+        timeline: 'December, 2018 - May 2020',
+        companyName: 'INVARIANT TELECOM',
+        designition: 'Software Engineer',
+        location: 'Mohakhali, Dhaka, Bangladesh',
+        link: 'https://invarianttele.com/',
+        details: 'I have already involved in 3 wonderful service like Dhamaka Digital, BUDDY and Wallet Service . Dhamaka is Multivendor Ecommerce Solution. Buddy is a ride sharing service and Wallet is a financial service. I contributed myself working with various technology like NodeJS, GraphQL, ExpressJS, MongoDB, VueJS. Build Restfull api and Back office pannels'
+      },
+      {
+        timeline: 'March, 2018 - November, 2018',
+        companyName: 'DOCTORKOI SMART',
+        designition: 'Full Stack Javascript Developer',
+        location: 'Mohammadpur, Dhaka, Bangladesh',
+        link: 'https://www.doctorkoi.com/',
+        details: 'Doctorkoi SMART is prescription management software that helps Doctors to generate prescriptions within just few clicks. It requires few seconds to prepare a full prescription by using SMART. Doctors can customize prescription according to their needs. Medicines can be selected from 26,000+ registered drug lists. So there is no chance to make any mistake. I worked as a full stack javascript developer. ReactJS, Featherjs, ElectronJS and many more things are used'
+      },
+      {
+        timeline: 'January, 2017 - February, 2018',
+        companyName: 'UY SYSTEM LTD',
+        designition: 'Web Developer',
+        link: 'https://www.uysys.com/',
+        location: 'Mohakhali, Dhaka, Bangladesh',
+        details: 'Its an outsourcing software company. I have done multiple projects of outsourcing clients. Project of tourism management like tripzip.tours and Ecommerce Solution is done by me . NodeJS , Laravel , ExpressJS, Mysql , Mongodb are used for providing solutions'
+      }],
+      projects: [
+        {
+            title: 'Dhamaka Digital',
+            subtitle: 'Multivendor Ecommerce',
+            image: '/img/dhamaka.png',
+            details: 'Dhamaka Shopping is backed by MicroTrade Group and the first Bangladeshi e-commerce platform with exclusive live feature for merchants and customer as well. Through this feature customer can find trendy, seasonal, occasional desired products 365x24x7 with live videos. Supported by a wide range of products Dhamaka Shopping aims to bring unique payment system, friendly customer experience and wonderful shopping experience with easy return policy.',
+            contribution: 'Worked as Teamlead and built graphql based architecture'
+        },
+        {
+            title: 'Buddy Ltd',
+            subtitle: 'Car Pooling Ride Sharing App',
+            image: '/img/buddy.png',
+            details: 'Buddy is an upcoming ride-sharing platform which you can utilize to make the daily commutes much easier. We provide carpooling service to your desired destination so that you can be punctual and economical at the same time.',
+            contribution: 'Build Restfull API By Javascript Base Plateform NodeJS.'
+        },
+        {
+            title: 'DoctorKoi Smart',
+            image: '/img/prescription.png',
+            subtitle: 'Prescription Generating Desktop App',
+            details: 'Doctorkoi SMART is prescription management software that helps Doctors to generate prescriptions within just few clicks. It requires few seconds to prepare a full prescription by using SMART. Doctors can customize prescription according to their needs. Medicines can be selected from 26,000+ registered drug lists. So there is no chance to make any mistake. All the prescriptions with patient history will be saved for future reference. The best thing is you don’t need any internet connection to use the software. The application works fully offline. Finally, this is an auto learning software, so if a Doctor uses the software, it will be automatically catered for that specific user.',
+            contribution: 'My Contribution behind the app is building and upgrading new features with ReactJS, ElectronJS and FeatherJS.'
+        }, {
+            title: 'Tripzip Tours',
+            image: '/img/tripzip.png',
+            subtitle: 'Tour Managment, Hotel & Homestay Booking Web App',
+            details: 'Travel Booking, Hotel Booking, Resort Booking, Tour Packages Booking, Online Booking and Payment, Adventure Tours, Activity Tours, Youth Special Tours, Cultural Tours, Budget Hotel Booking.',
+            contribution: 'My contribution is buidling App with Php/Laravel.'
+
+        },
+        {
+            title: 'Amar Ride',
+            subtitle: 'Car/Bike Ride Sharing APP',
+            image: '/img/512x512bb.jpg',
+            details: 'Amar Ride app is the easiest and fastest way to book a motor bike or a car in Bangladesh. It is the most popular bike & car booking service in Dhaka. Amar Ride is a ridesharing app for fast, reliable rides in a moment—day or night. There’s no need to wait for a taxi or bus. With Amar ride, you just tap to request a ride, and it’s easy to pay with credit or cash in select cities.',
+            contribution: 'My contribution is buidling Restfull api with NodeJS Express.'
+        }
+    ]
+}
